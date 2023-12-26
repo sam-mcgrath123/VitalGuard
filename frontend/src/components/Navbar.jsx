d00231041208ef32f81cdffae8547bec9241538b
Sam <sgm@iastate.edu>
2023-12-26 10:15:25 -0600

    adjusted to have blue navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,30 +6,28 @@ import {NavLink} from 'react-router-dom';
 function Navbar() {
 	return (
 		<div>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				<NavLink className="nav-link ms-sm-3 me-lg-5" to="/"><i className="bi bi-activity h3"></i></NavLink>
+			<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+				<NavLink className="navbar-brand ms-sm-3 me-lg-5" to="/">
+					<i className="bi bi-activity"></i>
+				</NavLink>
 
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav mr-auto">
+					<ul className="navbar-nav me-auto">
 						<li className="nav-item active me-lg-5">
 							<NavLink className="nav-link" to="/">
-								<p className="h5">Dashboard</p>
+								Dashboard
 							</NavLink>
 						</li>
 						<li className="nav-item active me-lg-3">
 							<NavLink className="nav-link" to="/">
-								<p className="h5">Rooms & Patients</p>
+								Rooms & Patients
 							</NavLink>
 						</li>
 					</ul>
 				</div>
-				<form className="row ms-auto">
-					<div className="col">
-						<input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-					</div>
-					<div className="col-auto me-sm-2">
-						<button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-					</div>
+				<form className="d-flex ms-auto">
+					<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+					<button className="btn btn-outline-light me-2" type="submit">Search</button>
 				</form>
 			</nav>
 		</div>
